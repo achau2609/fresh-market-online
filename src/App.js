6084f369f3a09bcfdaa20369e2d81e3bbe691e44
Adrian <>
2024-02-28 19:08:17 -0500

    Preliminary multipage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import { Route, Routes, BrowserRouter as Router, Link } from 'react-router-dom';
+import Home from "./pages";
+import About from './pages/about';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <div>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+            </ul>
+          </div>
+          <Routes>
+              
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/about" element={<About />} />
+          </Routes>
+      </Router>
   );
 }
 
