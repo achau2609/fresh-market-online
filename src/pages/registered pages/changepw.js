c9716d85b6f015fa859d31b2cb2a8a2bd3d651da
Adrian <>
2024-03-05 14:16:09 -0500

    myaccount progress

---
+++ src/pages/registered pages/changepw.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const ChangePw = () => {
+
+    return (
+        <>
+            <div>
+                My Orders
+                My Account
+            </div>
+            <div>
+                Account Details
+                <input className="payment-box-discount-code"></input>
+                Name:
+                <input className="payment-box-discount-code"></input>
+                Email:
+                <input className="payment-box-discount-code"></input>
+                Address:
+                <input className="payment-box-discount-code"></input>
+                <button className="myacc-action-btn">Save Changes</button>
+                <span>
+                    <button className="myacc-action-btn">Change Password</button>
+                    <button className="myacc-action-btn">Add Address</button>
+                </span>
+                
+            </div>
+        </>
+    )
+
+}
+
+export default ChangePw
--- src/pages/registered pages/myaccount.js
+++ src/pages/registered pages/myaccount.js
@@ -1,18 +1,53 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MyAccount = () => {
 
     return (
         <>
-            <div>
-
+            
+            <div className='myacc-navbar'>
+                <span className='myacc-navbar-btn'>My Orders</span>
+                <span className='myacc-navbar-btn'><Link to="/myaccount" style={{ textDecoration: 'none', color: "black"}}>My Account</Link></span>
             </div>
-            <div>
-                Account Details
-                Name:
-                Email:
-                Address:
-                <button>Save Changes</button>
+            <div className='myacc-details-box'>
+                <div style={{padding: "10px 0px 10px 20px", margin: "20px", fontSize: "20px"}}>
+                    Account Details
+                </div>
+                
+                <div className='myacc-details-input'>
+                    <div className='myacc-details-input-label'>
+                        <label for="fullname">Name: </label>
+                    </div>
+                    <div style={{margin: "auto auto", width: "80%"}}>
+                        <input type="text" id="fullname" name="fullname" className="myacc-details-input-box"></input><br />
+                    </div>
+                </div>
+
+                <div className='myacc-details-input'>
+                    <div className='myacc-details-input-label'>
+                        <label for="email">Email: </label>
+                    </div>
+                    <div style={{margin: "auto auto", width: "80%"}}>
+                        <input type="text" id="email" name="email" className="myacc-details-input-box"></input><br />
+                    </div>
+                </div>
+
+                <div className='myacc-details-input'>
+                    <div className='myacc-details-input-label'>
+                        <label for="address">Address: </label>
+                    </div>
+                    <div style={{margin: "auto auto", width: "80%"}}>
+                        <input type="text" id="address" name="address" className="myacc-details-input-box"></input><br />
+                    </div>
+                </div>
+
+                <button className="myacc-action-btn">Save Changes</button>
+                <span style={{float: "right"}}>
+                    <button className="myacc-action-btn">Change Password</button>
+                    <button className="myacc-action-btn">Add Address</button>
+                </span>
+                
             </div>
         </>
     )
