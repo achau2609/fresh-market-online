b8ab39eaf15b385a6631e6605f30c289196419f9
Adrian <>
2024-03-06 19:55:35 -0500

    fixed some links

--- src/component/publicView/Footer.js
+++ src/component/publicView/Footer.js
@@ -11,20 +11,18 @@ const Footer = () => {
                     <div className="col-12 col-md-3 mb-2">
                         <h4>CUSTOMER SERVICE</h4>
                         <div className='row mb-2'><Link to='/contactus' className='text-decoration-none'>Contact Us</Link></div>
-                        <div className='row mb-2'><Link to='#faq' className='text-decoration-none'>FAQ</Link></div>
-                        <div className='row mb-2'><Link to='#return' className='text-decoration-none'>Return & Refund</Link></div>
-                        <div className='row mb-2'><Link to='#payment' className='text-decoration-none'>Payment Methods</Link></div>
-                        <div className='row mb-2'><Link to='#tracking' className='text-decoration-none'>Order Tracking</Link></div>
+                        <div className='row mb-2'><Link to='/myaccount/orderhistory' className='text-decoration-none'>Order Tracking</Link></div>
                     </div>
+                    {/*}
                     <div className="col-12 col-md-3 mb-2">
                         <h4>SERVICES</h4>
                         <div className='row mb-2'><Link to='#return-policy' className='text-decoration-none'>Return Policy</Link></div>
                         <div className='row mb-2'><Link to='#delivery' className='text-decoration-none'>Delivery Policy</Link></div>
-                    </div>
+                    </div>*/}
                     <div className="col-12 col-md-3 mb-2">
                         <h4>ABOUT</h4>
                         <div className='row mb-2'><Link to='/aboutus' className='text-decoration-none'>About Us</Link></div>
-                        <div className='row mb-2'><Link to='#privacy' className='text-decoration-none'>Privacy Policy</Link></div>
+                        <div className='row mb-2'><Link to='#faq' className='text-decoration-none'>FAQ</Link></div>
                     </div>
                     <div className="col-12 col-md-3 mb-2">
                         <h4>PAYMENT</h4>
