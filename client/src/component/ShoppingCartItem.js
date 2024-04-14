851d3a9b1df51cf94944cc3562488e03883f1ad4
leon5678hk <149350355+leon5678hk@users.noreply.github.com>
2024-04-15 07:00:21 +0800

    added thumbnail redirecting

--- client/src/component/ShoppingCartItem.js
+++ client/src/component/ShoppingCartItem.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useCart } from './CartContext'
+import { Link } from 'react-router-dom';
 
 const ShoppingCartItem = ({ product }) => {
     const { addToCart, removeFromCart } = useCart();
@@ -12,11 +13,15 @@ const ShoppingCartItem = ({ product }) => {
         removeFromCart(product);
     };
 
+
     return (
         <div className='container'>
             <div className='row'>
                 <div className='col-12 col-sm-3'>
-                    <img src={product.Picture[0]} alt={product.ProductName} className='product-thumbnail' />
+                    <Link to={`/productlist/${encodeURIComponent(product.ProductName)}`}>
+                        <img src={product.Picture[0]} alt={product.ProductName} className='product-thumbnail' />
+                    </Link>
+
                 </div>
                 <div className='col-12 col-sm-4'>
                     <div className='row mb-2'>
@@ -27,7 +32,7 @@ const ShoppingCartItem = ({ product }) => {
                     </div>
                 </div>
                 <div className='col-12 col-sm-3'>
-                <button className='rounded-circle border border-custom-primary bg-light-subtle text-custom-primary me-3 p-1 px-2' onClick={handleRemoveFromCart}>
+                    <button className='rounded-circle border border-custom-primary bg-light-subtle text-custom-primary me-3 p-1 px-2' onClick={handleRemoveFromCart}>
                         {product.Quantity === 1 ?
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
@@ -46,7 +51,7 @@ const ShoppingCartItem = ({ product }) => {
                 </div>
                 <div className='col-12 col-sm-2'>
                     <div className='row justify-content-center'>
-                    ${Math.round((product.ProductPrice * product.Quantity + Number.EPSILON) * 100) / 100}
+                        ${Math.round((product.ProductPrice * product.Quantity + Number.EPSILON) * 100) / 100}
                     </div>
                 </div>
             </div>
